e93db87e9234b071297872afe24f9506b1a6cafc
KazuoYuuka <107776019+KazuoID@users.noreply.github.com>
2024-08-31 12:37:43 +0700

    add

---
+++ Kalkulator-testing-1/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Kalkulator_testing_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBox_1 = new System.Windows.Forms.TextBox();
+            this.txtBox_2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblHasil = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Angka 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Angka 2:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtBox_1
+            // 
+            this.txtBox_1.Location = new System.Drawing.Point(99, 34);
+            this.txtBox_1.Name = "txtBox_1";
+            this.txtBox_1.Size = new System.Drawing.Size(190, 20);
+            this.txtBox_1.TabIndex = 3;
+            // 
+            // txtBox_2
+            // 
+            this.txtBox_2.Location = new System.Drawing.Point(99, 79);
+            this.txtBox_2.Name = "txtBox_2";
+            this.txtBox_2.Size = new System.Drawing.Size(190, 20);
+            this.txtBox_2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Hasil";
+            // 
+            // lblHasil
+            // 
+            this.lblHasil.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblHasil.Location = new System.Drawing.Point(99, 128);
+            this.lblHasil.Name = "lblHasil";
+            this.lblHasil.Size = new System.Drawing.Size(167, 23);
+            this.lblHasil.TabIndex = 6;
+            this.lblHasil.Text = "output";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(54, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 37);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(126, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(66, 37);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(54, 229);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(66, 37);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "X";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(126, 229);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(66, 37);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "/";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(198, 186);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 80);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "C";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 314);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblHasil);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBox_2);
+            this.Controls.Add(this.txtBox_1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mini Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBox_1;
+        private System.Windows.Forms.TextBox txtBox_2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblHasil;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Kalkulator-testing-1/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulator_testing_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtBox_1.Text) || string.IsNullOrEmpty(txtBox_2.Text))
+            {
+                MessageBox.Show("Angka 1 dan Angka 2 Harus Terisi");
+            }
+            else {
+                int angka1 = int.Parse(txtBox_1.Text);
+                int angka2 = int.Parse(txtBox_2.Text);
+                int hasil = angka1 + angka2;
+                string hasilString = Convert.ToString(hasil);
+                lblHasil.Text = hasilString;
+            }
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtBox_1.Clear();
+            txtBox_2.Clear();
+            lblHasil.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtBox_1.Text) || string.IsNullOrEmpty(txtBox_2.Text))
+            {
+                MessageBox.Show("Angka 1 dan Angka 2 Harus Terisi");
+            }
+            else {
+                int a,b,c;
+                a = int.Parse(txtBox_1.Text);
+                b = int.Parse(txtBox_2.Text);
+                c = a - b;
+                this.lblHasil.Text = c.ToString();
+            }
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtBox_1.Text) || string.IsNullOrEmpty(txtBox_2.Text))
+            {
+                MessageBox.Show("Angka 1 dan Angka 2 Harus Terisi");
+            }
+            else {
+                int d, f, h;
+                d = int.Parse(txtBox_1.Text);
+                f = int.Parse(txtBox_2.Text);
+                h = d / f;
+                lblHasil.Text = f.ToString();
+            }
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            txtBox_1.Clear();
+            txtBox_2.Clear();
+            lblHasil.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtBox_1.Text) || string.IsNullOrEmpty(txtBox_2.Text))
+            {
+                MessageBox.Show("Angka 1 dan Angka 2 Harus Terisi");
+            }
+            else {
+                int a, b, c;
+                a = int.Parse(txtBox_1.Text);
+                b = int.Parse(txtBox_2.Text);
+                c = a * b;
+                lblHasil.Text = c.ToString();
+            }
+
+        }
+    }
+}
